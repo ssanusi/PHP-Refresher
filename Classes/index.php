af7ffd778406759bf9c71a94d426131686a8f122
sulaimansanusi <sulaiman.sanusi@icloud.com>
2020-04-14 02:16:40 +0100

    implemented classes and object

---
+++ Classes/index.php
@@ -0,0 +1,35 @@
+<?php
+
+class Task {
+    public string $description;
+    public bool $completed = false;
+
+    /**
+     * Task constructor.
+     * @param $description
+     */
+    public function __construct($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @param bool $completed
+     */
+    public function setCompleted(bool $completed): void
+    {
+        $this->completed = $completed;
+    }
+
+}
+
+$tasks = [
+    new Task('Take my Bath'),
+    new Task('Read my Books'),
+    new Task('Write Code')
+];
+
+$tasks[0]->setCompleted(true);
+
+
+require 'index.view.php';
---
+++ Functions/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'function.php';
+
+$animals = [
+    'dog',
+    'cat',
+    'goat'
+];
+
+
+dd($animals);
+
+require 'index.view.php';
