aa7c2d593ea18a726e46a7832ba73f745eaebf8e
sulaimansanusi <sulaiman.sanusi@icloud.com>
2020-04-18 05:28:00 +0100

    implemented drying up view

--- PDO/views/index.view.php
+++ PDO/views/index.view.php
@@ -1,31 +1,15 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PDO Demo</title>
-</head>
-<body>
-<nav>
-    <ul>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">contact</a></li>
-    </ul>
-</nav>
-<div>
-    <ul>
-        <?php foreach ($todos as $todo): ?>
-           <li>
-               <?php if ($todo->completed): ?>
-                   <strike> <?= $todo->description ?></strike>
-               <?php else: ?>
-                    <?= $todo->description ?>
-               <?php endif; ?>
-           </li>
-        <?php endforeach; ?>
-    </ul>
-</div>
-</body>
-</html>
+<?php require('partials/head.php') ?>
+    <div>
+        <ul>
+            <?php foreach ($todos as $todo): ?>
+                <li>
+                    <?php if ($todo->completed): ?>
+                        <strike> <?= $todo->description ?></strike>
+                    <?php else: ?>
+                        <?= $todo->description ?>
+                    <?php endif; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+<?php require('partials/footer.php') ?>
